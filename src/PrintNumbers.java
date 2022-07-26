4e436c1c78d03d68472ec7f3e53d99d407dfdb2a
Pradeep Reddy <pradeepreddy2512@gmail.com>
2022-07-26 20:35:29 +0530

    added gitignore

--- src/PrintNumbers.java
+++ src/PrintNumbers.java
@@ -2,7 +2,6 @@ public class PrintNumbers {
     public static void main(String[] args) {
         int n = 5;
         int initialization = 1;
-        // Print Numbers Question 2
         for (int i = initialization; i < n; i++) {
             for (int j = 0; j < n - 1; j++) {
                 System.out.print(" " + i + " ");
