0ff07947baa8047dfecc90ac0a0ebbcc78a2e26c
Pradeep Reddy <pradeepreddy2512@gmail.com>
2022-08-21 13:49:16 +0530

    added common

--- src/App.java
+++ src/App.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+      try {
         System.out.println("hello");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("scanner ==> "+ scanner);
+        int value = scanner.nextInt();
+        System.out.println("value ==> "+ value +"" + scanner);
+      } catch (Exception e) {
+        System.out.println("Error in Parsing ");
+      }
     }
 }
